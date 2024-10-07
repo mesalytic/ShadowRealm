315d3e9541d1e28c4871ee0984c3dc4d90350df1
Alice <electronixxyt5656@gmail.com>
2024-10-07 19:32:18 +0200

    🚧 chore: added dotenv integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("net.dv8tion:JDA:5.1.2")
+    implementation("io.github.cdimascio:dotenv-java:3.0.0")
 }
 
 application {
